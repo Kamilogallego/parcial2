d62f4824a734fa0dddd3970b168cc74df606d5b3
Kamilogallego <132090917+Kamilogallego@users.noreply.github.com>
2023-10-17 10:31:45 -0500

    CentroComercialDTO.java

--- ejercicioparcial/CentroComercialDTO.java
+++ ejercicioparcial/CentroComercialDTO.java
@@ -1,8 +1,48 @@
-package ejercicio2;public class CentroComercialDTO {
-    public <Integer, String> CentroComercialDTO(Integer idp, String nom, String cat, String ca) {
+package ejercicio2;
+
+import java.io.Serializable;
+
+public class CentroComercialDTO implements Serializable {
+    public Integer Id;
+    private String nombreCentro;
+    private String ciudad;
+    private String direcciom;
+
+    public CentroComercialDTO(Integer id, String nombreCentro, String ciudad, String direcciom) {
+        Id = id;
+        this.nombreCentro = nombreCentro;
+        this.ciudad = ciudad;
+        this.direcciom = direcciom;
     }
 
-    public int getId() {
-        return 0;
+    public Integer getId() {
+        return Id;
     }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getNombreCentro() {
+        return nombreCentro;
+    }
+
+    public void setNombreCentro(String nombreCentro) {
+        this.nombreCentro = nombreCentro;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getDirecciom() {
+        return direcciom;
+    }
+
+    public void setDirecciom(String direcciom) {
+        this.direcciom = direcciom;}
 }
